f7e2924ec9b83a2d3769637b598092268c69c1de
Kevin Huang <kevwhuang@gmail.com>
2022-10-11 20:44:29 -0500

    first commit

---
+++ scripts.js
@@ -0,0 +1,32 @@
+let currentMarker = 'X'
+
+const handleClick = (element) => {
+    console.log(`The element you clicked on has an id: ${element.id}`)
+    if (!document.getElementById(element.id).innerHTML) {
+        addMarker(element.id)
+    }
+}
+
+const addMarker = (id) => {
+    console.log(`The current marker is: ${currentMarker}`)
+    console.log(`A "${currentMarker}" should be placed in the square with the id: ${id}`)
+    document.getElementById(id).innerHTML = currentMarker
+    changeMarker()
+}
+const changeMarker = () => {
+    if (currentMarker === "X") {
+        currentMarker = "O"
+    }
+    else {
+        currentMarker = "X"
+    }
+}
+
+const resetBoard = () => {
+    const squares = document.getElementsByTagName("td");
+    console.log(squares)
+    for (i = 0; i < squares.length; i++) {
+        console.log(squares[i].id)
+        squares[i].innerHTML = null
+    }
+}
